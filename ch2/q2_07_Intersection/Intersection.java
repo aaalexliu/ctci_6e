a66a23459ba268ab53f5b71d0b01fcefaa28c6eb
alexsiqingliu <uilxela7@gmail.com>
2020-04-27 22:47:34 +0800

    finished intersection, ctci method far more complicated and saves on space but not time

---
+++ ch2/q2_07_Intersection/Intersection.java
@@ -0,0 +1,44 @@
+package ch2.q2_07_Intersection;
+
+import java.util.HashSet;
+
+import ch2.LinkedListNode;
+
+public class Intersection {
+
+  public static LinkedListNode findIntersection(
+    LinkedListNode head1, LinkedListNode head2) {
+    
+    HashSet<LinkedListNode> list1Set = new HashSet<LinkedListNode>();
+    
+    while (head1 != null) {
+      list1Set.add(head1);
+      head1 = head1.next;
+    }
+
+    while (head2 != null) {
+      if (list1Set.contains(head2)) return head2;
+      head2 = head2.next;
+    }
+
+    return new LinkedListNode(-1);
+  }
+
+  public static void main(String[] args) {
+    	/* Create linked list */
+		int[] vals = {-1, -2, 0, 1, 2, 3, 4, 5, 6, 7, 8};
+		LinkedListNode list1 = LinkedListNode.createLinkedListFromArray(vals);
+		
+		int[] vals2 = {12, 14, 15};
+		LinkedListNode list2 = LinkedListNode.createLinkedListFromArray(vals2);
+		
+		list2.next.next = list1.next.next.next.next;
+		
+		System.out.println(list1.printForward());
+		System.out.println(list2.printForward());
+  
+    LinkedListNode intersection = findIntersection(list1, list2);
+
+    System.out.println(intersection.printForward());
+  }
+}
